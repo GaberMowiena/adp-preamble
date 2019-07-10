c0462b1648fbf6bf7bfac0225db43a8631344e8c
= <gaber.abouelsoud@gmail.com>
2019-07-09 21:54:59 -0400

    adding Eslint

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const app = express();
 const bodyParser = require("body-parser")
 const request = require("request")
 
-app.use(bodyParser.urlencoded({
-    extended: true
-  }))
+// app.use(bodyParser.urlencoded({
+//     extended: true
+//   }))
 
-  app.use(bodyParser.json())
+//   app.use(bodyParser.json())
 
 
-
+// this is where an HTTP Get request is taking place using request library
 app.get('/', (req, res) => {
     request("https://interview.adpeai.com/api/v1/get-task", { json: true }, (error, response, body) => {
+        // here i'm manipulationg the returned body and implementing the operations and saving it in a variable to be used later in the post request
         let result = implementOps(body)
         request.post('https://interview.adpeai.com/api/v1/submit-task', { json: result }, (error, res, body) => {
             if (error) {
@@ -24,8 +25,21 @@ app.get('/', (req, res) => {
     )});
     })
 
-
-
+// this is in order to keep doing the same process continuosly ....
+// app.get('/stream', (req, res) => {
+//     while(true){
+//         request("https://interview.adpeai.com/api/v1/get-task", { json: true }, (error, response, body) => {
+//         let result = implementOps(body)
+//         request.post('https://interview.adpeai.com/api/v1/submit-task', { json: result }, (error, res, body) => {
+//             if (error) {
+//                 console.log("error ", error)
+//                 return
+//             }
+//             console.log(body)
+//         }
+//     )});
+//     }
+// })
 
 const implementOps = (obj) => {
     let objToPost = {}
@@ -55,8 +69,6 @@ const implementOps = (obj) => {
     }
     return objToPost
 }
-
-
 
 
 app.listen(3000,() => { 
